8a895703274e5cfa0c210de122f3190e74f2c08f
madhushathennakoon <madhushasampath@gmail.com>
2024-08-31 19:07:45 +0530

    implement train routes

---
+++ train_routes.js
@@ -0,0 +1,28 @@
+const routes = {
+  colomboToKandy: [
+    { name: "Colombo Fort", coordinates: [79.8507, 6.9355] },
+    { name: "Gampaha", coordinates: [79.9946, 7.0912] },
+    { name: "Polgahawela", coordinates: [80.2181, 7.3296] },
+    { name: "Rambukkana", coordinates: [80.3863, 7.3032] },
+    { name: "Kandy", coordinates: [80.6337, 7.2906] },
+  ],
+  colomboToGalgamuwa: [
+    { name: "Colombo Fort", coordinates: [79.8507, 6.9355] },
+    { name: "Gampaha", coordinates: [79.9946, 7.0912] },
+    { name: "Veyangoda", coordinates: [80.0194, 7.1976] },
+    { name: "Polgahawela", coordinates: [80.2181, 7.3296] },
+    { name: "Kurunegala", coordinates: [80.3548, 7.4863] },
+    { name: "Maho", coordinates: [80.4676, 7.8271] },
+    { name: "Galgamuwa", coordinates: [80.5274, 7.9906] },
+  ],
+  colomboToMatara: [
+    { name: "Colombo Fort", coordinates: [79.8487, 6.9355] },
+    { name: "Mount Lavinia", coordinates: [79.8653, 6.8327] },
+    { name: "Kalutara  ", coordinates: [79.9615, 6.5857] },
+    { name: "Balapitiya ", coordinates: [80.0319, 6.3708] },
+    { name: "Galle ", coordinates: [80.2154, 6.0335] },
+    { name: "Matara ", coordinates: [80.548, 5.9487] },
+  ],
+};
+
+module.exports = routes;
